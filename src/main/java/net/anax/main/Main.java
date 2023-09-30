0d2e975e6256ea7df47717f6c3bf09bc1d3fd588
Anax378 <frantisek.mrazek06@seznam.cz>
2023-09-30 23:15:29 +0200

    move port to a config file

--- src/main/java/net/anax/main/Main.java
+++ src/main/java/net/anax/main/Main.java
@@ -1,14 +1,24 @@
 package net.anax.main;
 
 import net.anax.thread.ListenerThread;
+import net.anax.thread.WorkerThread;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
+import java.io.ByteArrayOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.ServerSocket;
 
 public class Main {
 
-    private static final int port = 8080;
-    public static void main(String[] args) {
+    private static int port;
+    public static void main(String[] args) throws IOException, ParseException {
+
+        JSONObject config = (JSONObject)new JSONParser().parse(new FileReader("config.json"));
+        port = ((Long)config.get("port")).intValue();
         try {
             ServerSocket serverSocket = new ServerSocket(port);
             ListenerThread listenerThread = new ListenerThread(serverSocket);
